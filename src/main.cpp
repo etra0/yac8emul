5526b2584b810ee75000b1dd61e5bd8eeb4fe637
Sebastián Aedo <sebastian.aedo@sansano.usm.cl>
2021-04-13 17:24:25 -0400

    Added executable

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 ﻿// yac8emul.cpp : Defines the entry point for the application.
 //
 
-#include <yac8emul/yac8emul.h>
 #include <yac8emul/interpreter.h>
+#include <yac8emul/yac8emul.h>
 
-int main()
-{
+int main() {
   yac8emul::cpu cpu{};
-	std::cout << "Hello CMake." << std::endl;
-	return 0;
+
+  cpu.parse_instruction(0x0000);
+  std::cout << "Hello CMake." << std::endl;
+  return 0;
 }
