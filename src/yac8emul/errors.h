dba95fc903a0d189851636c67fd4ffa9ac5c4ade
Sebastián Aedo <sebastian.aedo@sansano.usm.cl>
2021-04-18 15:58:51 -0400

    Improved error handling
    Improve error handling by creating a father class, and now previous
    errors inherits from it to catch internal yac8emul errors more easily
    when we don't require granularity.

--- src/yac8emul/errors.h
+++ src/yac8emul/errors.h
@@ -4,30 +4,39 @@
 namespace yac8emul {
 
 namespace errors {
-struct invalid_instruction : public std::exception {
+
+struct error : public std::exception {
+  const char *what() const throw() override { return "internal error"; }
+  virtual std::string get_info() {
+    std::string msg = error::what();
+    return msg;
+  };
+};
+
+struct invalid_instruction : public error {
   std::string kind;
   std::string value;
   std::string msg;
 
   invalid_instruction(std::string kind_, std::string value_)
       : kind(kind_), value(value_){};
-  const char *what() const throw() { return "invalid instruction"; }
+  const char *what() const throw() override { return "invalid instruction"; }
 
-  const char *get_info() {
+  std::string get_info() override {
     this->msg = "invalid instruction: " + kind + " " + value;
-    return this->msg.c_str();
+    return this->msg;
   }
 };
 
-struct not_implemented : public std::exception {
+struct not_implemented : public error {
   std::string instruction;
   std::string msg;
 
   not_implemented(std::string instruction_) : instruction(instruction_){};
-  const char *what() const throw() { return "not implemented"; }
-  const char *get_info() {
+  const char *what() const throw() override { return "not implemented"; }
+  std::string get_info() override {
     this->msg = "not implemented: " + instruction;
-    return this->msg.c_str();
+    return this->msg;
   }
 };
 
